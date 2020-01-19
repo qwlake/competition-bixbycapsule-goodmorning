80155b65e176be74a533e14f0657f69d713b4dfc
jungwoo <qwlake@gmail.com>
2020-01-19 19:12:10 +0900

    #6 버그 수정 및 app-launch 기능 추가
    - #5 버그 수정
    - 삼성 기본 알람 설정 기능 추가(불안정)

--- room1431.goodmorning/code/setTimers/setUpTimers.js
+++ room1431.goodmorning/code/setTimers/setUpTimers.js
@@ -4,12 +4,13 @@ var fail = require("fail")
 var utils = require('../Utils.js')
 
 // 지금 자서 일어날 시간 계산
-module.exports.function = function setUpTimers (sleepPeriod) {
-  console.log(sleepPeriod);
-  let zonedDT = utils.toZonedDateTime(sleepPeriod);
+module.exports.function = function setUpTimers (sleepPeriodHours, sleepPeriodMinutes) {
+  let zonedDT = dates.ZonedDateTime.now();
   let timerList = [];
+  let until = zonedDT.plusHours(sleepPeriodHours);
+  until.plusMinutes(sleepPeriodMinutes);
   zonedDT = zonedDT.plusMinutes(14);
-  while (dates.ZonedDateTime.now().isBefore(zonedDT)) { //
+  while (zonedDT.isBefore(until)) { //
     zonedDT = zonedDT.plusMinutes(90);
   }
   timerList.push({
@@ -17,5 +18,5 @@ module.exports.function = function setUpTimers (sleepPeriod) {
     reason:"일어날 시간",
     hasReason:true,});
   console.log(timerList);
-  return timerList;
+  return {timer:timerList};
 }
